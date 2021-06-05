2336c60a7048867230c48347310404802ca6c8fc
Miguel Arriaga <mta2122@columbia.edu>
2021-06-05 06:19:05 +0100

    update

--- scripts/load_folha.js
+++ scripts/load_folha.js
@@ -1,3 +1,23 @@
+
+function getRoot(){
+    const loc = window.location;
+    console.log(loc.protocol)
+    console.log(loc.hostname)
+    console.log(loc.pathname)
+
+    if (loc.hostname == "localhost"){return ""}
+
+    var path = loc.pathname
+    console.log(path)
+    if (path.endsWith(".html")) {
+        path = path.substr(0, path.lastIndexOf("/"));
+    }
+    console.log(path)
+
+    return loc.protocol + "//" + loc.hostname +"/" + path + "/";
+}
+
+
 function loadPage() {
 
 
